7538c304f62569bfea3ce36828168626c6ac6b47
Mohemmed <moh080203@gmail.com>
2024-08-29 17:49:22 +0200

    created project

---
+++ PG3302_02_Task1/Date.cs
@@ -0,0 +1,5 @@
+namespace PG3302_02_Task1;
+
+public class Date {
+    private string month;
+}
---
+++ PG3302_02_Task1/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace PG3302_02_Task1;
+
+class Program {
+    static void Main() {
+        Console.WriteLine("Hello, World!");
+    }
+}
