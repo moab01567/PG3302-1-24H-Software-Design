a63eb0e233162979f90b07d831ad55fa96f039c6
Mohemmed <moh080203@gmail.com>
2024-08-29 18:28:09 +0200

    created class Person

--- PG3302_02_Task1/Person.cs
+++ PG3302_02_Task1/Person.cs
@@ -2,5 +2,10 @@ namespace PG3302_02_Task1;
 
 public class Person
 {
+    private string _fistName;
+    private string _lastName;
     
+    public string FirstName {
+        set { _fistName = value;} get{return _fistName;} }
+    public string LastName { set{} get; }
 }
